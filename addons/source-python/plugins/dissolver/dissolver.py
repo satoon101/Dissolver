9b6492eb9476895ba5c308e20b98affe68daf621
Stephen Toon <satoon101@gmail.com>
2016-02-28 09:47:14 -0500

    Changed the retrieval of the ragdoll entity to instead use the player's m_hRagdoll property.
    Removed the game support validation on loading the plugin.
    
    Added a delay variable to allow delaying the dissolve by a set amount of seconds.
    
    Updated ConVar declarations to use new argument order in current version of SP.

--- addons/source-python/plugins/dissolver/dissolver.py
+++ addons/source-python/plugins/dissolver/dissolver.py
@@ -14,23 +14,23 @@ from warnings import warn
 # Source.Python Imports
 #   Config
 from config.manager import ConfigManager
-#   Cvars
-from cvars.flags import ConVarFlags
 #   Entities
 from entities.constants import DissolveType
+from entities.constants import INVALID_ENTITY_INTHANDLE
 from entities.entity import Entity
+from entities.helpers import index_from_inthandle
 #   Events
 from events import Event
-#   Filters
-from filters.entities import EntityIter
+#   Listeners
+from listeners.tick import Delay
 #   Players
-from players.helpers import inthandle_from_userid
+from players.entity import Player
+from players.helpers import index_from_userid
 #   Translations
 from translations.strings import LangStrings
 
 # Script Imports
-from dissolver import ragdoll_classname
-from dissolver.info import info
+from .info import info
 
 
 # =============================================================================
@@ -50,8 +50,7 @@ _config_strings = LangStrings(info.basename)
 with ConfigManager(info.basename, 'dissolver_') as _config:
 
     # Create the dissolver type cvar
-    dissolver_type = _config.cvar(
-        'type', 0, ConVarFlags.NONE, _config_strings['Type'])
+    dissolver_type = _config.cvar('type', 0, _config_strings['Type'])
 
     # Loop through all dissolver types
     for _name in DissolveType.__members__:
@@ -61,20 +60,23 @@ with ConfigManager(info.basename, 'dissolver_') as _config:
             getattr(DissolveType, _name).real, _name))
 
     # Add random and remove to the list of options
-    dissolver_type.Options.append('{0} = RANDOM'.format(_num_dissolve_types))
-    dissolver_type.Options.append(
-        '{0} = REMOVE'.format(_num_dissolve_types + 1))
+    for _num, _option in enumerate(('RANDOM', 'REMOVE')):
+        dissolver_type.Options.append('{0} = {1}'.format(
+            _num_dissolve_types + _num, _option))
 
     # Create the dissolver magnitude cvar
     dissolver_magnitude = _config.cvar(
-        'magnitude', 2, ConVarFlags.NONE, _config_strings['Magnitude'])
+        'magnitude', 2, _config_strings['Magnitude'])
+
+    # Create the delay cvar
+    dissolver_delay = _config.cvar('delay', 0, _config_strings['Delay'])
 
 
 # =============================================================================
 # >> GAME EVENTS
 # =============================================================================
 @Event('player_death')
-def dissolve_ragdoll(game_event):
+def dissolve_player_ragdoll(game_event):
     """Dissolve/remove the player's ragdoll on death."""
     # Get the type of dissolver to use
     current_type = dissolver_type.get_int()
@@ -86,31 +88,39 @@ def dissolve_ragdoll(game_event):
         warn('Invalid value for {0} cvar "{1}".'.format(
             dissolver_type.name, current_type))
 
+        # Use the remove setting
+        current_type = _num_dissolve_types + 2
+
+    # Delay the dissolving
+    Delay(
+        max(0, dissolver_delay.get_int()),
+        dissolve_ragdoll, game_event['userid'], current_type)
+
+
+# =============================================================================
+# >> HELPER FUNCTIONS
+# =============================================================================
+def dissolve_ragdoll(userid, current_type):
+    """Dissolve/remove the player's ragdoll."""
+    # Get the ragdoll entity
+    inthandle = Player(index_from_userid(userid)).ragdoll
+    if inthandle == INVALID_ENTITY_INTHANDLE:
+        return
+    entity = Entity(index_from_inthandle(inthandle))
+
     # Should the ragdoll just be removed?
     if current_type == _num_dissolve_types + 2:
+        entity.remove()
+        return
 
-        # Get the player's integer handle
-        inthandle = inthandle_from_userid(game_event.get_int('userid'))
-
-        # Loop through all current ragdolls
-        for entity in EntityIter(ragdoll_classname, return_types='entity'):
-
-            # Does the current one belong to the player?
-            if entity.owner == inthandle:
-
-                # Remove the ragdoll
-                entity.remove()
-
-                # No need to loop further
-                return
+    # Set the target name for the player's ragdoll
+    entity.target_name = 'ragdoll_{1}'.format(userid)
 
     # Get the dissolver entity
-    dissolver_entity = _get_dissolver_entity()
+    dissolver_entity = Entity.find_or_create('env_entity_dissolver')
 
     # Should a random dissolve type be chosen?
     if current_type == _num_dissolve_types + 1:
-
-        # Get a random dissolve type
         current_type = randrange(_num_dissolve_types)
 
     # Set the magnitude
@@ -120,19 +130,11 @@ def dissolve_ragdoll(game_event):
     dissolver_entity.dissolve_type = current_type
 
     # Dissolve the ragdoll
-    dissolver_entity.dissolve(ragdoll_classname)
+    dissolver_entity.dissolve('ragdoll_{1}'.format(userid))
 
 
-# =============================================================================
-# >> HELPER FUNCTIONS
-# =============================================================================
-def _get_dissolver_entity():
-    """Return an env_entity_dissolver instance."""
-    # Loop through all current dissolver entities on the server
-    for entity in EntityIter('env_entity_dissolver', return_types='entity'):
-
-        # Return the first entity
-        return entity
-
-    # If no dissolver entities exist, create one and return its instance
-    return Entity.create('env_entity_dissolver')
+@Event('player_say')
+def kill_player(game_event):
+    """Kill the player to create the ragdoll."""
+    player = Player(index_from_userid(game_event['userid']))
+    player.take_damage(player.health * 2)
--- addons/source-python/plugins/dissolver/info.py
+++ addons/source-python/plugins/dissolver/info.py
@@ -22,5 +22,4 @@ info.version = '1.0'
 info.basename = 'dissolver'
 info.variable = info.basename + '_version'
 info.url = ''
-info.convar = PublicConVar(
-    info.variable, info.version, 0, info.name + ' Version')
+info.convar = PublicConVar(info.variable, info.version, info.name + ' Version')
